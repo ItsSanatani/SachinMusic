12d8ba74c0c23c048aae4abaf0a80e1b9c0b4b4f
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-08 17:03:35 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- ChatBot/database/__init__.py
+++ ChatBot/database/__init__.py
@@ -2,13 +2,15 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import config
 
 ChatBot = AsyncIOMotorClient(config.MONGO_URL)
-db = ChatBot["ChattBot"]
 
+db = ChatBot["ChattBot"]
 usersdb = db["users"]
 chatsdb = db["chats"]
 sudodb = db["sudo"]
+authdb = db["authusers"]
 
 from .chats import *
 from .admin import *
 from .fsub import *
 from .sudo import *
+from .auth import *
