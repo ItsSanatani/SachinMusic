3d4373102e795d7c521eedc705764a92ae461787
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-08 19:48:51 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- ChatBot/database/auth.py
+++ ChatBot/database/auth.py
@@ -1,17 +1,21 @@
 from . import authdb
 
-async def add_auth(user_id: int):
-    user = await authdb.find_one({"user_id": user_id})
+# Add auth user for specific group
+async def add_auth(chat_id: int, user_id: int):
+    user = await authdb.find_one({"chat_id": chat_id, "user_id": user_id})
     if not user:
-        await authdb.insert_one({"user_id": user_id})
+        await authdb.insert_one({"chat_id": chat_id, "user_id": user_id})
 
-async def remove_auth(user_id: int):
-    await authdb.delete_one({"user_id": user_id})
+# Remove auth user from specific group
+async def remove_auth(chat_id: int, user_id: int):
+    await authdb.delete_one({"chat_id": chat_id, "user_id": user_id})
 
-async def get_auth_users():
-    users = await authdb.find().to_list(length=0)
+# Get all auth users for a specific group
+async def get_auth_users(chat_id: int):
+    users = await authdb.find({"chat_id": chat_id}).to_list(length=0)
     return [user["user_id"] for user in users]
 
-async def is_auth(user_id: int) -> bool:
-    user = await authdb.find_one({"user_id": user_id})
+# Check if a user is authorized in a specific group
+async def is_auth(chat_id: int, user_id: int) -> bool:
+    user = await authdb.find_one({"chat_id": chat_id, "user_id": user_id})
     return bool(user)
