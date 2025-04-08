a53b131cb1e1b99d06537b435a3331e3b16ff44c
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-08 20:53:31 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- ChatBot/__main__.py
+++ ChatBot/__main__.py
@@ -1,22 +1,16 @@
 import os
 import importlib
-import subprocess
 import asyncio
-
 from pyrogram import idle
 
 from ChatBot import app
 from ChatBot.modules import ALL_MODULES
-from config import WEB_APP  
 
 async def boot():
     await app.start()
 
     for module in ALL_MODULES:
         importlib.import_module(f"ChatBot.modules.{module}")
-
-    if WEB_APP:
-        subprocess.Popen(['python3', 'app.py'])
 
     await idle()
     await app.stop()
