eec0f8f8fe49d74d4ad07d8c8685f01981f39462
ItsSanatani <sachinxmeena2000@gmail.com>
2025-04-08 16:57:52 +0530

    ❖ | sᴧᴄʜɪɴ ꭙ sᴧɴᴧᴛᴧɴɪ | ❖

--- ChatBot/modules/start.py
+++ ChatBot/modules/start.py
@@ -4,7 +4,7 @@ import random
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.enums import ChatType
-
+import config
 from config import STICKER, FSUB, IMG, OWNER_ID
 from ChatBot import app
 from ChatBot.database import add_user, add_chat, get_fsub
